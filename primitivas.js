0178f5aaad267c435ea2715664b0379b916dc931
Mastercelta <juancamilo@bees.com.co>
2024-10-23 20:29:28 -0500

    Primer commit

---
+++ primitivas.js
@@ -0,0 +1,19 @@
+//ejemplo de tipos de datos primitivos
+//Autor: Juan Camilo Penaloza
+
+//String
+var nombre = "Juan Camilo";
+console.log(nombre);
+
+//Number
+var edad = 20;
+console.log(edad);
+
+//Boolean
+var esEstudiante = true;
+console.log(esEstudiante);
+
+//Array
+var materias = ["Calculo", "Fisica", "Quimica"];
+console.log(materias);
+console.log(materias[1]);
