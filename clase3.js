6aef12d5be4284c4d4ff5022b0727b304cd88d5d
Mastercelta <juancamilo@bees.com.co>
2024-10-30 19:12:51 -0500

    Clase 3

---
+++ clase3.js
@@ -0,0 +1,18 @@
+//Objetos
+const persona = {
+    nombre: 'Juan',
+    edad: 25,
+    trabaja:true
+};
+
+//Imprimir el objeto
+console.log(persona);
+//Imprimir una propiedad del objeto
+
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.trabaja);
+
+persona.pelo = "pelo";
+
+console.log(persona);
